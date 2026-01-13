75465bb7001c36c09915ec1e38997601882bff95
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-13 21:14:18 +0530

    q3 anwser

--- question-3/app.js
+++ question-3/app.js
@@ -1,6 +1,31 @@
-import {writeFile} from "fs/promises"
+const os = require("os");
+const fs =require("fs").promises;
 
-await writeFile("./data.txt","Hello World")
+
+async function main(){
+    //part A os module
+
+console.log("free Memory:",os.cpus());
+console.log("Total CPU Cores:",os.freemem());
+
+
+//part B is the file system curd
+//create the data.txt
+await  fs.writeFile("./data.txt","Hello World!!")
 console.log("saved")
+//create the  Readme file
+await fs.writeFile("Readme.md","##This is first line in Readme");
 
-await writeFile("Readme.md",)
+//read the data.txt
+const f= require("fs")
+const d=f.readFileSync("./data.txt",{encoding:"utf-8"});
+console.log(d);
+
+//append second line into data.txt
+await fs.appendFile("data.txt","This is second line");
+
+//delete the Readme.md
+await fs.unlink("Readme.md")
+
+}
+main().catch(console.error);
